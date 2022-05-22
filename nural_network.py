42d1015459f984f45f89c2c4ff9d4ed9b81559e2
shakedbarcessat <89984201+shakedbarcessat@users.noreply.github.com>
2022-05-22 12:03:08 +0300

    Add files via upload

--- nural_network.py
+++ nural_network.py
@@ -19,59 +19,115 @@ import numpy as np
 
 
 def run_model(train, test, validation):
+    """
+    The function normalizes li and returns it.
+    
+    
+    Parameters:    
+    ---------
+    input:
+    train- list of train
+    test- list of test
+    validation- list of validation
+    ---------
+    output:
+    Returns a normalized list.    
+    ---------
+    """
     model = define_model()
     model = train_model(model, train[0], train[1], validation[0], validation[1])
     evaluate_model(model, test[0], test[1])
 
 def define_model():
+    """
+    The function normalizes li and returns it.
+    
+    
+    Parameters:    
+    ---------
+    input:
+    li- the dataset.    
+    ---------
+    output:
+    Returns a normalized list.    
+    ---------
+    """
+    
     model = Sequential()
     model.add(Conv2D(16, (3, 3), 
-              activation='relu', 
-              kernel_initializer='he_uniform', 
-              input_shape=(100, 100, 3)))   
-    
+                 activation='relu', 
+                 kernel_initializer='he_uniform', 
+                 input_shape=(100, 100, 3)))   
+        
     model.add(MaxPooling2D((2, 2)))
-
+    
     model.add(Conv2D(32, (3, 3), 
-              activation='relu', 
-              kernel_initializer='he_uniform'))
-    
+                 activation='relu', 
+                 kernel_initializer='he_uniform'))
+        
     model.add(Dropout(0.2))
-    
-    model.add(Conv2D(16, (3, 3), 
-              activation='relu', 
-              kernel_initializer='he_uniform'))
-    
+        
     model.add(Conv2D(64, (3, 3), 
-              activation='relu', 
-              kernel_initializer='he_uniform'))
-    
-    model.add(Dropout(0.2))
+                 activation='relu', 
+                 kernel_initializer='he_uniform'))
+  
     model.add(Conv2D(32, (3, 3), 
-              activation='relu', 
-              kernel_initializer='he_uniform'))
-    
+                 activation='relu', 
+                 kernel_initializer='he_uniform'))
+        
     model.add(MaxPooling2D((2, 2)))
     model.add(Flatten())
-
+    
     model.add(Dense(1,
                     activation='sigmoid'))
-	
-    # compile model
-    opt = SGD(lr=0.01, momentum=0.9)
-    model.compile(optimizer=opt, 
-                  loss='binary_crossentropy', 
-                  metrics=['accuracy'])
-    
+    	
+    Adam = SGD(lr=0.01, momentum=0.9)
+    model.compile(optimizer=Adam, 
+                     loss='binary_crossentropy', 
+                     metrics=['accuracy'])
+        
     return model
 
 
 def train_model(model, trainX ,trainY, validationX, validationY):
-    model.fit(trainX, trainY, epochs=25, batch_size=256, validation_data=(validationX, validationY))
+    """
+    The function fits the model it gets and then reutrns it.
+    
+    
+    Parameters:    
+    ---------
+    input:
+    model- The model we fit.
+    trainX- the list of train.
+    trainY- The labels of the train data.
+    validationX- the list of validation.
+    validationY- The labels of the validation data.
+    ---------
+    output:
+    Returns the model after fitting it. 
+    ---------
+    """
+    model.fit(trainX, trainY, epochs=20, batch_size=256, validation_data=(validationX, validationY))
     
     return model
     
 def evaluate_model(model, testX, testY):
+    """
+    The function fits the model it gets and then prints the score.
+    
+    
+    Parameters: 
+    score-
+    ---------
+    input:
+    model-The model we fit.
+    testX-The list of test
+    testY- The labels of the test data.
+    ---------
+    output:
+    Prints the scores- accuracy and loss of the test fit.    
+    ---------
+    """
     score = model.evaluate(testX, testY, verbose=1)
     print(score)
 
