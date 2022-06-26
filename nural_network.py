b8da135dc6ac8d087fd288fb1054b329301355ac
shakedbarcessat <89984201+shakedbarcessat@users.noreply.github.com>
2022-06-26 11:24:27 +0300

    final

--- nural_network.py
+++ nural_network.py
@@ -6,16 +6,18 @@ Created on Sun Feb 13 11:17:00 2022
 """
 
 import nural_network
-from sklearn.model_selection import KFold
-from keras.models import Sequential
-from keras.layers import Conv2D
-from keras.layers import MaxPooling2D
-from keras.layers import Dropout
-from keras.layers import Dense
-from keras.layers import Flatten
-from keras.optimizers import SGD
-from numpy import array
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.optimizers import SGD
+
+import matplotlib.pyplot as plt
+
 import numpy as np
+
 
 
 def run_model(train, test, validation):
@@ -35,8 +37,11 @@ def run_model(train, test, validation):
     ---------
     """
     model = define_model()
-    model = train_model(model, train[0], train[1], validation[0], validation[1])
-    evaluate_model(model, test[0], test[1])
+    history = train_model(model, train[0], train[1], validation[0], validation[1])
+    evaluate_model(history, test[0], test[1])
+    
+    
+
 
 def define_model():
     """
@@ -85,7 +90,7 @@ def define_model():
     model.compile(optimizer=Adam, 
                      loss='binary_crossentropy', 
                      metrics=['accuracy'])
-        
+    
     return model
 
 
@@ -107,14 +112,36 @@ def train_model(model, trainX ,trainY, validationX, validationY):
     Returns the model after fitting it. 
     ---------
     """
-    model.fit(trainX, trainY, epochs=20, batch_size=256, validation_data=(validationX, validationY))
+    history = model.fit(trainX, trainY, epochs=2, batch_size=256, validation_data=(validationX, validationY))
     
-    return model
+
+    accuracy = history.history['accuracy']  # each epoch train accuracy
+    val_acc = history.history['val_accuracy']  # each epoch validation accuracy
     
+    loss = history.history['loss']  # each epoch train loss
+    val_loss = history.history['val_loss']  # each epoch validation loss
+    
+    epochs_range = range(2)  # a list containing numbers from 0 to the number of epochs-1
+    plt.figure(figsize=(8, 8))
+    
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, accuracy, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+    
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and validation loss')
+    plt.show()
+
+    return history
+
 def evaluate_model(model, testX, testY):
     """
     The function fits the model it gets and then prints the score.
-    
     
     Parameters: 
     score-
@@ -128,36 +155,13 @@ def evaluate_model(model, testX, testY):
     Prints the scores- accuracy and loss of the test fit.    
     ---------
     """
+    #history2 = model
+    #the history of accuracies and losses of each epoch throughout the learning phase
     score = model.evaluate(testX, testY, verbose=1)
     print(score)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # Generate a prediction using model.predict() 
+    # and calculate it's shape:    
+    print("Generate a prediction")
+    prediction = model.predict(testX[:1])
+    print("prediction shape:", prediction.shape)
+    #model.save('savedmodel/model.h5')
